aec2cceeee1fa53e7694f50bb413eaba94539767
emmanueldavidmartin <emmanuel.david.martin77@gmail.com>
2023-04-08 22:35:33 -0300

    aplicacion de usuarios

---
+++ app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>about</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Fugit asperiores maiores doloremque provident ab magnam 
+                odio placeat a quibusdam eaque corporis voluptate ducimus 
+                hic numquam quam mollitia, aliquid consequatur facilis, 
+                iste praesentium cum nulla fuga delectus! Impedit sequi 
+                deleniti accusamus placeat, inventore error labore, 
+                eligendi voluptas perspiciatis, quas voluptates fuga 
+                iure quis nostrum consectetur illo officiis nisi amet? 
+                Officiis rerum saepe commodi corporis voluptatum facere 
+                cum, qui nobis voluptas illum ex provident minus quia 
+                non omnis doloremque illo consequuntur, voluptatibus iure 
+                tenetur facilis explicabo. Magnam sapiente perferendis 
+                commodi ullam labore dolor facilis, asperiores incidunt. 
+                Voluptatibus, id adipisci! Pariatur, ad iste.</p>
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ServicesPage() {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Maiores expedita sit corporis ullam magnam sint maxime 
+                modi dolore libero similique magni quidem, 
+                iusto repellendus animi earum illo veniam alias 
+                consectetur saepe. Rerum repellat quos nemo ipsa
+                 debitis quo praesentium dolore a, nam itaque eaque 
+                 doloribus, quidem quisquam? Nostrum, officia soluta?</p>
+        </div>
+    );
+}
+
+export default ServicesPage;
